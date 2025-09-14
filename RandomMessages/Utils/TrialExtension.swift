32b9f606b673ca00ad4ea9a8cb8dee15bdc2924e
Pierre-Philippe CHARLIER <consulting@ppcharlier.be>
2025-09-14 23:43:53 +0200

    Code cleaning

--- RandomMessages/Utils/TrialExtension.swift
+++ RandomMessages/Utils/TrialExtension.swift
@@ -19,30 +19,30 @@ extension Trial {
         
     }
     
-    public var chart1B : String {
-        let rmg = RandomMessagesGenerator()
-        if let msg = self.result {
-            return  rmg.invCorrespondancePerChart(message: msg, chartType: .type1)
-        }else{
-            return ""
-        }
-    }
-    
-    public var chart2A : String {
-        let rmg = RandomMessagesGenerator()
-        if let msg = self.result {
-            return  rmg.correspondancePerChart(message: msg, chartType: .type2)
-        }else{
-            return ""
-        }
-    }
-    
-    public var chart2B : String {
-        let rmg = RandomMessagesGenerator()
-        if let msg = self.result {
-            return  rmg.invCorrespondancePerChart(message: msg, chartType: .type2)
-        }else{
-            return ""
-        }
-    }
+//    public var chart1B : String {
+//        let rmg = RandomMessagesGenerator()
+//        if let msg = self.result {
+//            return  rmg.invCorrespondancePerChart(message: msg, chartType: .type1)
+//        }else{
+//            return ""
+//        }
+//    }
+//    
+//    public var chart2A : String {
+//        let rmg = RandomMessagesGenerator()
+//        if let msg = self.result {
+//            return  rmg.correspondancePerChart(message: msg, chartType: .type2)
+//        }else{
+//            return ""
+//        }
+//    }
+//    
+//    public var chart2B : String {
+//        let rmg = RandomMessagesGenerator()
+//        if let msg = self.result {
+//            return  rmg.invCorrespondancePerChart(message: msg, chartType: .type2)
+//        }else{
+//            return ""
+//        }
+//    }
 }
